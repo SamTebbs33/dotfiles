965d730d1201d9fe7b7bc8a46d262defd29003a8
SamTebbs33 <samuel.tebbs@yahoo.com>
2018-08-01 13:35:08 +0100

    Added oh-my-fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,10 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
 for file in ~/.config/fish/*.fish
 	set filename (basename $file)
 	if [ "$filename" != "config.fish" ]; source $file; end
