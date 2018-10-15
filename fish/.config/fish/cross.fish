05363f498c7dccf42a866fc91228132bc7d9e965
Sam Tebbs <sam.tebbs@arm.com>
2018-10-15 11:51:57 +0100

    Fix wkspc tools path

--- fish/.config/fish/cross.fish
+++ fish/.config/fish/cross.fish
@@ -1,5 +1,5 @@
 if hostname | grep "e120" > /dev/null
   set DEBUG_FLAGS "-dp" "-da" "--save-temps -S"
   set WORKSPACE_HOME "/home/$USER/gnu-work/workspace"
-  set PATH "$WORKSPACE_HOME/tools" $PATH
+  set PATH "$WORKSPACE_HOME/tools/dist" "$WORKSPACE_HOME" $PATH
 end
