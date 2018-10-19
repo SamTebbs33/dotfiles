780b715055df0471ccccce46b43dc1726b131354
Sam Tebbs <sam.tebbs@arm.com>
2018-10-19 10:14:36 +0100

    Add aarch64-linux-gnu- toolchain to path

--- fish/.config/fish/cross.fish
+++ fish/.config/fish/cross.fish
@@ -2,4 +2,5 @@ if hostname | grep "e120" > /dev/null
   set DEBUG_FLAGS "-dp" "-da" "--save-temps -S"
   set WORKSPACE_HOME "/home/$USER/gnu-work/workspace"
   set PATH "$WORKSPACE_HOME/tools/dist" "$WORKSPACE_HOME" $PATH
+  set PATH "$HOME/bin/gcc-arm-8.2-2018.08-x86_64-aarch64-linux-gnu/bin" $PATH
 end
