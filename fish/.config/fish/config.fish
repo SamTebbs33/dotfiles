48bad548f26eeca7aebd9364bcabf6e38db37de2
Sam Tebbs <sam@sam-mbp-arch.localdomain>
2019-04-03 00:02:59 +0100

    Uninstall omf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
 for file in ~/.config/fish/*.fish
 	set filename (basename $file)
 	if [ "$filename" != "config.fish" ]; and [ "$filename" != "_*" ]; source $file; end
