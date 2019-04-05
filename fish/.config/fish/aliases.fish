799a320acb0ada9a40478541f134b3de51b15087
Sam Tebbs <sam.tebbs@arm.com>
2019-04-05 14:01:40 +0100

    Remove other unused aliases

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -12,22 +12,9 @@ end
 
 # Programs
 alias fnd "find . -name "
-alias grun 'java org.antlr.v4.runtime.misc.TestRig'
-alias antlr4 'java -jar /usr/local/lib/antlr-4.5-complete.jar'
-alias mkalias "java -jar ~/Documents/Programming/MkAlias.jar"
-alias dirtag "java -jar ~/Documents/Programming/DirTag.jar"
-alias gcci386 "~/Documents/Programming/cross/toolchain/gcc/i686-pc-elf/bin/gcc -I $HOME/Documents/Programming/cross/toolchain/gcc/lib/gcc/i686-pc-elf/4.6.2/include"
-alias maple "java -jar $MAPLE_PATH"
-alias ash "java -jar $ASH_PATH"
-
-# Uni SSH
-alias unisync "rsync -aP ~/Dropbox/University/ sxt567@tinky-winky.cs.bham.ac.uk:work"
 
 # ls
 alias ls 'ls --color=auto -alh -I "." -I ".."'
-alias lsr "ls -R"
-alias realls "/bin/ls"
-
 function lss
     ls $argv | less
 end
@@ -35,16 +22,11 @@ end
 # Less
 alias less "less -R"
 
-# Cd
-alias cdgit "cd $GIT_REPOS"
-alias cddl "cd ~/Downloads"
-
 # Other
 alias . "open ."
 alias .. 'cd ..'
 alias mk "mkfile 0"
 alias cls "clear"
-alias starwars "telnet towel.blinkenlights.nl"
 if [ (uname) = "Linux" ]
 	alias aupdate "sudo apt-get update"
 	alias arm "sudo apt-get remove"
@@ -56,7 +38,6 @@ end
 
 # Path
 set PATH "/home/$USER/bin" $PATH
-set PATH "/home/$USER/bin/zig-linux-x86_64-0.3.0+c7b9ab98" $PATH
 
 # grep
 function grp
