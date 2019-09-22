0491ffbc961a004f00e8e42a61946c34199fdddb
SamTebbs33 <samuel.tebbs@gmail.com>
2019-09-22 10:07:07 +0100

    Add fish paths

---
+++ fish/.config/fish/path.fish
@@ -0,0 +1,6 @@
+set PATH /home/$USER/bin/gcc/i686-elf/build/bin $PATH
+set PATH /home/$USER/bin/binutils/i686-elf/build/bin $PATH
+set PATH /home/$USER/bin/grub/build/bin $PATH
+set PATH /home/$USER/bin/llvm/bin $PATH
+set PATH /home/$USER/bin/clang/bin $PATH
+set PATH /home/$USER/repos/zig/build/bin $PATH
