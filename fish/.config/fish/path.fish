bd9e73362eb246a7152d16c9cda03fa34dd8ff94
Sam Tebbs <samuel.tebbs@yahoo.com>
2019-10-06 19:57:03 +0100

    Add ruby gems to path

--- fish/.config/fish/path.fish
+++ fish/.config/fish/path.fish
@@ -4,3 +4,4 @@ set PATH /home/$USER/bin/grub/build/bin $PATH
 set PATH /home/$USER/bin/llvm/build/bin $PATH
 set PATH /home/$USER/bin/clang/build/bin $PATH
 set PATH /home/$USER/repos/zig/build/bin $PATH
+set PATH /home/$USER/.gem/ruby/2.6.0/bin $PATH
