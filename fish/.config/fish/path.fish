4432f683698ae57bb310c050497224362815d887
Sam Tebbs <samuel.tebbs@yahoo.com>
2019-10-14 21:41:06 +0100

    Add llvm-8 build to PATH

--- fish/.config/fish/path.fish
+++ fish/.config/fish/path.fish
@@ -2,6 +2,7 @@ set PATH /home/$USER/bin/gcc/i686-elf/build/bin $PATH
 set PATH /home/$USER/bin/binutils/i686-elf/build/bin $PATH
 set PATH /home/$USER/bin/grub/build/bin $PATH
 set PATH /home/$USER/bin/llvm/build/bin $PATH
+set PATH /home/$USER/bin/llvm/build-8.0.0/bin $PATH
 set PATH /home/$USER/bin/clang/build/bin $PATH
 set PATH /home/$USER/repos/zig/build/bin $PATH
 set PATH /home/$USER/.gem/ruby/2.6.0/bin $PATH
