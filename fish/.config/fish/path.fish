fdbfd18a95dfbca99597eee4afcf00dab8c83d2c
Sam Tebbs <samuel.tebbs@gmail.com>
2020-06-15 15:51:32 +0100

    Check for dir existence before adding to path

--- fish/.config/fish/path.fish
+++ fish/.config/fish/path.fish
@@ -1,8 +1,14 @@
-set PATH /home/$USER/bin/gcc/i686-elf/build/bin $PATH
-set PATH /home/$USER/bin/binutils/i686-elf/build/bin $PATH
-set PATH /home/$USER/bin/grub/build/bin $PATH
-set PATH /home/$USER/bin/llvm/build/bin $PATH
-set PATH /home/$USER/bin/llvm/build-8.0.0/bin $PATH
-set PATH /home/$USER/bin/clang/build/bin $PATH
-set PATH /home/$USER/repos/zig/build/bin $PATH
-set PATH /home/$USER/.gem/ruby/2.6.0/bin $PATH
+function add_bin_dir -a dir
+	if test -d $dir
+		set PATH $dir $PATH
+	end
+end
+
+add_bin_dir /home/$USER/bin/gcc/i686-elf/build/bin
+add_bin_dir /home/$USER/bin/binutils/i686-elf/build/bin
+add_bin_dir /home/$USER/bin/grub/build/bin
+add_bin_dir /home/$USER/bin/llvm/build/bin
+add_bin_dir /home/$USER/bin/llvm/build-8.0.0/bin
+add_bin_dir /home/$USER/bin/clang/build/bin
+add_bin_dir /home/$USER/repos/zig/build/bin
+add_bin_dir /home/$USER/.gem/ruby/2.6.0/bin
