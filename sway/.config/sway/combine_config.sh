2605d0ecda1dcf274bddd1003c041b0d8d0757f2
Sam Tebbs <samuel.tebbs@gmail.com>
2022-06-01 16:13:35 +0100

    all: remove sway, spicetify, tmux and fonts

--- sway/.config/sway/combine_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat ~/.config/sway/config.i3config > ~/.config/sway/config
-if [ -f ~/.config/sway/device.i3config ]; then
-	cat ~/.config/sway/device.i3config >> ~/.config/sway/config
-fi
