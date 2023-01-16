d47e9b9cea4175b628b3980a2daa1add4e814b98
Samuel Tebbs <samuel.tebbs@arm.com>
2023-01-16 12:14:13 +0000

    fish: add exit code to prompt

--- fish/.config/fish/_prompt.fish
+++ fish/.config/fish/_prompt.fish
@@ -1,4 +1,9 @@
 function fish_prompt
+    set -l s $status
+    set -l status_str ""
+    if [ $s != 0 ]
+        set status_str (printf " ("; set_color brred; printf "$s"; set_color normal; printf ")")
+    end
 	set -l name (set_color brblue; printf (whoami); set_color normal)
 	set -l host (set_color brblue; printf (hostname); set_color normal)
 	set -l at (set_color brwhite; printf "@"; set_color normal)
@@ -6,7 +11,7 @@ function fish_prompt
 	set -l dir (set_color brcyan; printf (echo $dir | sed 's|'$HOME'|~|g'); set_color normal)
 	set -l dir (set_color brcyan; printf (echo $dir | sed 's|gnu-work/workspace|wkspc|g'); set_color normal)
 	set -l time (set_color brcyan; printf (date +"%H:%M"); set_color normal)
-	echo "($time) $name$at$host ($dir)"
+	echo "($time) $name$at$host ($dir)$status_str"
     echo "→ "
 end
 
